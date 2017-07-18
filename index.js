cb19c83e7b7d6bfbece363791ccda9071f5e7c13
github username <github email address>
2017-07-18 17:04:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 // Add your doToElementsInArray() function here:
 
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
+
+var animals = ["dog", "cat", "squirrel"];
+
 
 // Add your changeCompletely() function here:
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
+
+var animals = ["dog", "cat", "squirrel"];
+
+doToElementsInArray(animals, changeCompletely);
